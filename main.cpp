326a7d8e6610f26a7b6c03fc42ad793b7a7628b0
Felix <felix.t.hoppe@gmail.com>
2025-11-01 07:10:18 -0400

    Add a space after "What is your name?"

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 std::string name;
 int main() {
 	std::cout << "Hello, World!" << std::endl;
-	std::cout << "What is your name?";
+	std::cout << "What is your name? ";
 	getline(std::cin, name);
 	std::cout << "Hello, " << name << "!" << std::endl;
 	return 0;
