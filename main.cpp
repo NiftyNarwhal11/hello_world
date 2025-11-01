1ae4300009ba71c4572d7a8652bd8d746b4e99b3
Felix <felix.t.hoppe@gmail.com>
2025-11-01 07:06:33 -0400

    Made it interactive.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 
+std::string name;
 int main() {
 	std::cout << "Hello, World!" << std::endl;
+	std::cout << "What is your name?";
+	getline(std::cin, name);
+	std::cout << "Hello, " << name << "!" << std::endl;
 	return 0;
 }
